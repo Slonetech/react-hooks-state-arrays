9fed1d5c7bff149cce36c63e307b0f8c996563d2
Slonetech <kengeregordon@gmail.com>
2023-07-22 09:04:21 +0300

    competed assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,17 +6,53 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
+    const newFoodList = [...foods, newFood];
+    setFoods(newFoodList);
     console.log(newFood);
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  
+
+function handleLiClick(id) {
+  const updatedFoods = foods.map((food) => {
+    if (food.id === id) {
+      return {
+        ...food,
+        heatLevel: food.heatLevel + 1,
+      };
+    } else {
+      return food;
+    }
+  });
+
+  setFoods(updatedFoods);
+}
+  const[filterBy, setFilterBy ] = useState("ALL");
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "ALL") {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  });
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
-
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value)``
+  };
   return (
     <div>
+    <select name="filter" onChange={handleFilterChange}>
+      <option value="ALL">All</option>
+      <option value="American">American</option>
+      <option value="Sichuan">Sichuan</option>
+      <option value="Thai">Thai</option>
+      <option value="Mexican">Mexican</option>
+    </select>
+  
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
